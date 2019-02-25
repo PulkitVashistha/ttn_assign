d6263c8de9eadaaf086a30d45ecad33c868df7f1
ShubhamKumarSA <shubhamkumarsa@outlook.com>
2019-02-25 20:46:32 +0530

    Logic Updated

--- JavaSession4/src/Question9.java
+++ JavaSession4/src/Question9.java
@@ -1,21 +1,25 @@
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class Question9 {
     
     public static void main(String[] args) {
     
-        Date D=new Date();
-        SimpleDateFormat SDF;
         
-        SDF=new SimpleDateFormat("YYYY-MM-dd");
-        System.out.println("YMD: "+SDF.format(D));
-        
-        SDF=new SimpleDateFormat("dd-MM-YYYY");
-        System.out.println("DMY: "+SDF.format(D));
+        Date date=new Date();
+        Locale locale=new Locale("en","in");
+        DateFormat timeFormat=DateFormat.getTimeInstance(DateFormat.FULL,locale);
+        DateFormat dateFormat=DateFormat.getDateInstance(DateFormat.FULL,locale);
+        System.out.println("India\nCurrent Date: "+dateFormat.format(date)+" Current Time: "+timeFormat.format(date));
     
-        SDF=new SimpleDateFormat("MM-dd-YYYY");
-        System.out.println("MDY: "+SDF.format(D));
+        Locale locale1=new Locale("it","ch");
+        DateFormat timeFormat1=DateFormat.getTimeInstance(DateFormat.FULL,locale1);
+        DateFormat dateFormat1=DateFormat.getDateInstance(DateFormat.FULL,locale1);
+        System.out.println("Italian\nCurrent Date: "+dateFormat1.format(date)+" Current Time: "+timeFormat1.format(date));
+        
+        
         
         
     }
